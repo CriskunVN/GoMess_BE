c1edaf9e184ecbe0175fd9cf960f8540fdb5d28b
CrisKunVN <phukun0404@gmail.com>
2025-11-22 18:20:04 +0700

    docs: add swagger docs

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,10 @@ import cookieParser from "cookie-parser";
 import AppError from "./utils/AppError.js";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import { protect } from "./middlewares/protectAuth.middleware.js";
+import swagger from "swagger-ui-express";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 // import routes
 import AuthRoute from "./routes/auth.route.js";
@@ -21,12 +25,25 @@ import FriendRoute from "./routes/friend.route.js";
 import MessageRoute from "./routes/message.route.js";
 import ConversationRoute from "./routes/conversation.route.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const CLIENT_URL: string = process.env.CLIENT_URL as string;
 //1. Middleware
 app.use(cors({ origin: CLIENT_URL, credentials: true })); // CORS cho phép truy cập từ client
 app.use(morgan("dev")); // Ghi log các request
 app.use(cookieParser());
 app.use(express.json());
+
+// Swagger API documentation
+const swaggerDocument = JSON.parse(
+  readFileSync(join(__dirname, "../src/swagger.json"), "utf-8")
+);
+app.use(
+  "/api-docs",
+  swagger.serve,
+  swagger.setup(swaggerDocument, { explorer: true })
+);
 
 //2. Routes
 const apiPrefix = process.env.API_PREFIX;
